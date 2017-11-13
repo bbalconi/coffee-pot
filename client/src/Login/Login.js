21c5cac2c13eba5687f898c89cab9cee5f90a6d9
bbalconi <bbalconi@gmail.com>
2017-11-13 15:54:32 -0700

    Early stage server routing.. signup/login

---
+++ client/src/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Col, Button, CardSubtitle, FormGroup, Label, Input, Card, CardBody, CardTitle } from 'reactstrap';
+import { withRouter } from 'react-router-dom';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}

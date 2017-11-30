da5f311abd045d8a654f413ba02f9fc996d0e050
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-11-30 13:59:35 -0700

    added styles for responsive web view on mobile

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -7,20 +7,21 @@ export default class Footer extends Component {
   }
   render() {
     return (
-      <div style={{ justifyContent: 'space-between', backgroundColor: '#333', paddingTop: 30, paddingBottom: 30, paddingLeft: '1.5em', paddingRight: '1.5em', color: '#fff'}}>
-        <div style={{display: 'flex'}}><div style={{flex: 1}}>
-          <ul>
-            <li><Link style={{color: '#ccc'}} to="/">Coffee Pot Pi</Link></li>
-            <li><Link style={{color: '#ccc'}} to="/login">Login</Link></li>
-            <li><Link style={{color: '#ccc'}} to="/signup">Signup</Link></li>
-            <li><Link style={{color: '#ccc'}} to="/about">About</Link></li>
-          </ul>
-        </div>         
-        <div>Last brew time: <strong>10:30 a.m.</strong></div>
-        <div style={{flex: 1, textAlign:'right'}}>
-          <Link to="/"><img style={{maxWidth: 200}} src="/images/logo-inverted.png" alt="Coffee Pot Pi"/></Link>
+      <div className="footer" style={{ justifyContent: 'space-between', backgroundColor: '#333', paddingTop: 30, paddingBottom: 30, paddingLeft: '1.5em', paddingRight: '1.5em', color: '#fff'}}>
+        <div className="footer-container container" style={{display: 'flex'}}>
+          <div style={{flex: 1}}>
+            <ul>
+              <li><Link style={{color: '#ccc'}} to="/">Coffee Pot Pi</Link></li>
+              <li><Link style={{color: '#ccc'}} to="/login">Login</Link></li>
+              <li><Link style={{color: '#ccc'}} to="/signup">Signup</Link></li>
+              <li><Link style={{color: '#ccc'}} to="/about">About</Link></li>
+            </ul>
+          </div>         
+          <div>Last brew time: <strong>10:30 a.m.</strong></div>
+          <div className="footer-logo">
+            <Link to="/"><img style={{maxWidth: 200}} src="/images/logo-inverted.png" alt="Coffee Pot Pi"/></Link>
+          </div>
         </div>
-      </div>
       </div>
     )
   }
